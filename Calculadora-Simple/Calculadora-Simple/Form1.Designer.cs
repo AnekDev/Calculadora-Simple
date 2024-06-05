6a37433354de1c5586aa2e5aa7b3dc8f0e0442ef
ManelDaza <127338918+ManelDaza@users.noreply.github.com>
2024-06-05 19:00:56 +0200

    Mostrar números
    En esta actualización logro que en la calculadora se enseñen los números que se están marcando

---
+++ Calculadora-Simple/Calculadora-Simple/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Calculadora_Simple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Resultado = new System.Windows.Forms.Label();
+            this.number1 = new System.Windows.Forms.Button();
+            this.number2 = new System.Windows.Forms.Button();
+            this.number3 = new System.Windows.Forms.Button();
+            this.number4 = new System.Windows.Forms.Button();
+            this.number5 = new System.Windows.Forms.Button();
+            this.number6 = new System.Windows.Forms.Button();
+            this.number7 = new System.Windows.Forms.Button();
+            this.number8 = new System.Windows.Forms.Button();
+            this.number9 = new System.Windows.Forms.Button();
+            this.number0 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Resultado
+            // 
+            this.Resultado.AutoSize = true;
+            this.Resultado.Location = new System.Drawing.Point(323, 78);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(14, 16);
+            this.Resultado.TabIndex = 0;
+            this.Resultado.Text = "0";
+            this.Resultado.Click += new System.EventHandler(this.Resultado_Click);
+            // 
+            // number1
+            // 
+            this.number1.Location = new System.Drawing.Point(189, 139);
+            this.number1.Name = "number1";
+            this.number1.Size = new System.Drawing.Size(77, 67);
+            this.number1.TabIndex = 1;
+            this.number1.Text = "1";
+            this.number1.UseVisualStyleBackColor = true;
+            this.number1.Click += new System.EventHandler(this.number1_Click);
+            // 
+            // number2
+            // 
+            this.number2.Location = new System.Drawing.Point(289, 139);
+            this.number2.Name = "number2";
+            this.number2.Size = new System.Drawing.Size(77, 67);
+            this.number2.TabIndex = 2;
+            this.number2.Text = "2";
+            this.number2.UseVisualStyleBackColor = true;
+            this.number2.Click += new System.EventHandler(this.number2_Click);
+            // 
+            // number3
+            // 
+            this.number3.Location = new System.Drawing.Point(398, 139);
+            this.number3.Name = "number3";
+            this.number3.Size = new System.Drawing.Size(77, 67);
+            this.number3.TabIndex = 3;
+            this.number3.Text = "3";
+            this.number3.UseVisualStyleBackColor = true;
+            this.number3.Click += new System.EventHandler(this.number3_Click);
+            // 
+            // number4
+            // 
+            this.number4.Location = new System.Drawing.Point(505, 139);
+            this.number4.Name = "number4";
+            this.number4.Size = new System.Drawing.Size(77, 67);
+            this.number4.TabIndex = 4;
+            this.number4.Text = "4";
+            this.number4.UseVisualStyleBackColor = true;
+            this.number4.Click += new System.EventHandler(this.number4_Click);
+            // 
+            // number5
+            // 
+            this.number5.Location = new System.Drawing.Point(88, 246);
+            this.number5.Name = "number5";
+            this.number5.Size = new System.Drawing.Size(77, 67);
+            this.number5.TabIndex = 5;
+            this.number5.Text = "5";
+            this.number5.UseVisualStyleBackColor = true;
+            this.number5.Click += new System.EventHandler(this.number5_Click);
+            // 
+            // number6
+            // 
+            this.number6.Location = new System.Drawing.Point(189, 246);
+            this.number6.Name = "number6";
+            this.number6.Size = new System.Drawing.Size(77, 67);
+            this.number6.TabIndex = 6;
+            this.number6.Text = "6";
+            this.number6.UseVisualStyleBackColor = true;
+            this.number6.Click += new System.EventHandler(this.number6_Click);
+            // 
+            // number7
+            // 
+            this.number7.Location = new System.Drawing.Point(289, 246);
+            this.number7.Name = "number7";
+            this.number7.Size = new System.Drawing.Size(77, 67);
+            this.number7.TabIndex = 7;
+            this.number7.Text = "7";
+            this.number7.UseVisualStyleBackColor = true;
+            this.number7.Click += new System.EventHandler(this.number7_Click);
+            // 
+            // number8
+            // 
+            this.number8.Location = new System.Drawing.Point(398, 246);
+            this.number8.Name = "number8";
+            this.number8.Size = new System.Drawing.Size(77, 67);
+            this.number8.TabIndex = 8;
+            this.number8.Text = "8";
+            this.number8.UseVisualStyleBackColor = true;
+            this.number8.Click += new System.EventHandler(this.number8_Click);
+            // 
+            // number9
+            // 
+            this.number9.Location = new System.Drawing.Point(505, 246);
+            this.number9.Name = "number9";
+            this.number9.Size = new System.Drawing.Size(77, 67);
+            this.number9.TabIndex = 9;
+            this.number9.Text = "9";
+            this.number9.UseVisualStyleBackColor = true;
+            this.number9.Click += new System.EventHandler(this.number9_Click);
+            // 
+            // number0
+            // 
+            this.number0.Location = new System.Drawing.Point(88, 139);
+            this.number0.Name = "number0";
+            this.number0.Size = new System.Drawing.Size(77, 67);
+            this.number0.TabIndex = 10;
+            this.number0.Text = "0";
+            this.number0.UseVisualStyleBackColor = true;
+            this.number0.Click += new System.EventHandler(this.number0_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(637, 383);
+            this.Controls.Add(this.number0);
+            this.Controls.Add(this.number9);
+            this.Controls.Add(this.number8);
+            this.Controls.Add(this.number7);
+            this.Controls.Add(this.number6);
+            this.Controls.Add(this.number5);
+            this.Controls.Add(this.number4);
+            this.Controls.Add(this.number3);
+            this.Controls.Add(this.number2);
+            this.Controls.Add(this.number1);
+            this.Controls.Add(this.Resultado);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Name = "Form1";
+            this.Text = "CalculadoraSimple";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Resultado;
+        private System.Windows.Forms.Button number1;
+        private System.Windows.Forms.Button number2;
+        private System.Windows.Forms.Button number3;
+        private System.Windows.Forms.Button number4;
+        private System.Windows.Forms.Button number5;
+        private System.Windows.Forms.Button number6;
+        private System.Windows.Forms.Button number7;
+        private System.Windows.Forms.Button number8;
+        private System.Windows.Forms.Button number9;
+        private System.Windows.Forms.Button number0;
+    }
+}
+
---
+++ Calculadora-Simple/Calculadora-Simple/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_Simple
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Resultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numbers()
+        {
+            
+            Resultado.Text += $"{i}";
+        }
+
+        private void number0_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            numbers();
+        }
+
+        private void number1_Click(object sender, EventArgs e)
+        {
+            i = 1;
+            numbers();
+        }
+
+        private void number2_Click(object sender, EventArgs e)
+        {
+            i = 2;
+            numbers();
+        }
+
+        private void number3_Click(object sender, EventArgs e)
+        {
+            i = 3;
+            numbers();
+        }
+
+        private void number4_Click(object sender, EventArgs e)
+        {
+            i = 4;
+            numbers();
+        }
+
+        private void number5_Click(object sender, EventArgs e)
+        {
+            i = 5;
+            numbers();
+        }
+
+        private void number6_Click(object sender, EventArgs e)
+        {
+            i = 6;
+            numbers();
+        }
+
+        private void number7_Click(object sender, EventArgs e)
+        {
+            i = 7;
+            numbers();
+        }
+
+        private void number8_Click(object sender, EventArgs e)
+        {
+            i = 8;
+            numbers();
+        }
+
+        private void number9_Click(object sender, EventArgs e)
+        {
+            i = 9;
+            numbers();
+        }
+    }
+}
